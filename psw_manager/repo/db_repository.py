7ff46b252e0fcc821e23e4ffc342c6aca39e4380
Artem Borovik <borovikao@mail.ru>
2023-02-20 19:39:05 +0600

    update

--- psw_manager/repo/db_repository.py
+++ psw_manager/repo/db_repository.py
@@ -1,5 +1,8 @@
+import sqlite3
 
 PATH = ''
+CONNECTION = None
+CURSOR = None
 
 def read() -> dict:
 	return {}
@@ -12,3 +15,13 @@ def delete(Resource) -> str:
 
 def update(Resource, resource_old) -> str:
 	return 'Resource update was successful'
+
+def connect_db():
+	global CONNECTION, CURSOR
+	if CONNECTION == None:
+		try:
+			CONNECTION = sqlite3.connect('./psw_manager.db')
+			CURSOR = CONNECTION.cursor()	
+		except:
+			print('Failed to connect to database. Check your connection string')
+		
